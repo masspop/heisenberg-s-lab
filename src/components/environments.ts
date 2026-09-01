8ae35ce953b4ef0d27fcda5160b9042042fc4ca1
masspop <asimaydemir1441@gmail.com>
2026-09-01 15:40:09 +0300

    Update environments.ts

--- src/components/environments.ts
+++ src/components/environments.ts
@@ -1,7 +1,7 @@
 import type { ReactElement } from "react";
 interface SceneProps {
   className?: string;
-}
+}m
 type SceneComponent = (props: SceneProps) => ReactElement;
 export function HotdogCartScene({ className = "" }: SceneProps) {
   return (
